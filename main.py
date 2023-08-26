0f455c48fbcf91705dbe07222dd25729551a5588
Isaac Korankye Asante <xquisit31@gmail.com>
2023-08-26 18:34:37 +0000

    this is gradually working

--- main.py
+++ main.py
@@ -4,8 +4,5 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @ app.get("/")
-def read_root():
-    return {
-        "success": True,
-        "message": "Welcome to Sling Academy Public API"
-    } 
+def index():
+    return {"name": "First Data"}
