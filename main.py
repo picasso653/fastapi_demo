eaf535170692364f31024249c71772eb23116ede
Isaac Korankye Asante <xquisit31@gmail.com>
2023-08-28 21:51:03 +0000

    fff

--- main.py
+++ main.py
@@ -15,5 +15,14 @@ def index():
     return {"name": "First Data"}
 
 @app.get("/get-student/{student_id}")
-def get_student(student_id: int = Path(None, description= "The ID fo the student you want to view")):
+def get_student(student_id: int = Path(None,
+ description= "The ID fo the student you want to view")):
     return students[student_id]
+
+
+@app.get("/get-by-name")
+def get_student(name: str):
+    for student_id in students:
+        if students[student_id]["name"] == name:
+            return students[student_id]
+        return {"Data": "Not Found"}
