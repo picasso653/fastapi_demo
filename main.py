ac0f4c0d9376d840c2fd40f288cee9a74c05fc83
Isaac Korankye Asante <xquisit31@gmail.com>
2023-08-31 03:02:42 +0000

    We'll deal with this next time

--- main.py
+++ main.py
@@ -2,18 +2,16 @@
 from fastapi import FastAPI, Path
 
 app = FastAPI()
-students = {
-    1: {
-        "name" : "John",
-        "age": 17,
-        "class": "Year 12"
-    }
-}
-
-@ app.get("/")
-def index():
-    return {"name": "First Data"}
-
-@app.get("/get-student/{student_id}")
-def get_student(student_id: int = Path(None, description= "The ID of the students you want to view")):
-    return students[student_id]
+@app.get("/todos")
+def get_todos(db):
+    todos = []
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM todos")
+    for row in cursor:
+        todo = {
+            "id": row[0],
+            "task": row[1],
+            "completed": row[2],
+        }
+        todos.append(todo)
+    return todos
